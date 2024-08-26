5db8a8fd96efa58c5acc647188d5cd4ae78368be
akshata120 <avhadakshata12@gmail.com>
2024-08-26 16:20:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+let boxes = document.querySelectorAll(".box");
+
+let turn = "X";
+let isGameOver = false;
+
+boxes.forEach(e => {
+    e.innerHTML = "";
+    e.addEventListener("click", () => {
+        if (!isGameOver && e.innerHTML === "") {
+            e.innerHTML = turn;
+            checkWin();
+            checkDraw();
+            changeTurn();
+        }
+    });
+});
+
+function changeTurn() {
+    if (turn === "X") {
+        turn = "O";
+        document.querySelector(".bg").style.left = "85px";
+    } else {
+        turn = "X";
+        document.querySelector(".bg").style.left = "0";
+    }
+}
+
+function checkWin() {
+    let winConditions = [
+        [0, 1, 2], // Top row
+        [3, 4, 5], // Middle row
+        [6, 7, 8], // Bottom row
+        [0, 3, 6], // Left column
+        [1, 4, 7], // Middle column
+        [2, 5, 8], // Right column
+        [0, 4, 8], // Diagonal
+        [2, 4, 6]  // Diagonal
+    ];
+
+    for(let i=0; i<winConditions.length; i++){
+        let v0 = boxes[winConditions[i][0]].innerHTML;
+        let v1 = boxes[winConditions[i][1]].innerHTML;
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if(v0 != "" && v0 === v1 && v0 === v2){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = turn + " win ";
+            document.querySelector("play-again").style.display = "inline";
+        }
+    }
+}
+
+function checkDraw() {
+}
